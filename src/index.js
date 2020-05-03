071db8cf9e37a10965cd085595643ea716082922
saturajala <satu.rajala@hotmail.fi>
2020-05-04 01:27:25 +0300

    Week 4 lesson

--- src/index.js
+++ src/index.js
@@ -2,14 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './components/Login';
+import Register from './components/Register';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class AppRouter extends React.Component {
+
+  render() {
+    return(
+      <Router>
+        <div className="app">
+
+          <nav className="main-nav">
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </nav>
+
+          <Switch>
+            <Route path="/" exact component={App} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/register" exact component={Register} />
+          </Switch>
+
+        </div>
+      </Router>
+    );
+  }
+
+}
+
+ReactDOM.render( <AppRouter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
