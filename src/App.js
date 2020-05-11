d173c34a0e592658b9fd0d8258219016fd5f41c3
saturajala <satu.rajala@hotmail.fi>
2020-05-12 00:23:47 +0300

    Lesson week 6

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import Chatbox from './components/Chatbox';
 import { Link } from 'react-router-dom';
+import firebase from './firebase';
 /* import Hello from './Hello'; */
 
 class App extends React.Component {
@@ -10,21 +11,35 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      term: '',
-      items: []
+      message: ''
+      /* term: '' */
+      /* items: [] */
     };
   }
 
   onChange = (event) => {
-    this.setState({ term: event.target.value });
+    /* this.setState({ term: event.target.value }); */
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   onSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
+    if(this.state.message !== ''){
+      const chatRef = firebase.database().ref('general');
+      const chat = {
+        message: this.state.message,
+        user: this.props.user.displayName,
+        timestamp: new Date().getTime()
+      }
+
+      chatRef.push(chat);
+      this.setState({message: ''});
+    }
+/*     event.preventDefault();
     this.setState({
       term: '',
       items: [...this.state.items, this.state.term]
-    });
+    }); */
   }
 
   render() {
@@ -34,9 +49,14 @@ class App extends React.Component {
 
         {this.props.user &&
           <div className="allow-chat">
-            <Chatbox items={this.state.items} />
+            <Chatbox /* items={this.state.items} */ />
             <form className="message-form" onSubmit={this.onSubmit}>
-              <input value={this.state.term} onChange={this.onChange} />
+              <input 
+              type="text"
+              name="message"
+              id="message"
+              value={this.state.message} 
+              onChange={this.onChange} />
               <button>Send</button>
             </form>
           </div>
